062194fa167932bf41a877c88555191c57a79963
BarmanInfo <barmaninfo1991@gmail.com>
2015-10-02 16:04:34 +0530

    Commit 02-10-2015

---
+++ RoundedProgressBarMat/gen/com/akexorcist/roundcornerprogressbar/example/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.akexorcist.roundcornerprogressbar.example;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
